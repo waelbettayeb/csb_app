66d7010775690d4d344653275ca54fd14e725195
wael <khelilwael@gmail.com>
2019-05-11 23:21:55 +0100

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,140 @@
+// Copyright 2018-present the Flutter authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:flutter/material.dart';
+
+import 'backdrop.dart';
+import 'colors.dart';
+import 'home.dart';
+import 'menu_page.dart';
+import 'model/product.dart';
+import 'supplemental/cut_corners_border.dart';
+import 'package:flutter/services.dart';
+
+class CSBApp extends StatefulWidget {
+  @override
+  _CSBAppState createState() => _CSBAppState();
+}
+
+class _CSBAppState extends State<CSBApp> {
+  Category _currentCategory = Category.home;
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'CSB',
+      debugShowCheckedModeBanner: false,
+      home: Backdrop(
+        currentCategory: _currentCategory,
+        frontLayer: HomePage(category: _currentCategory),
+        backLayer: MenuPage(
+          currentCategory: _currentCategory,
+          onCategoryTap: _onCategoryTap,
+        ),
+        frontTitle: Text(
+          'CSB',
+          style: TextStyle(
+              color:kCSBWhite,
+              fontWeight: FontWeight.w800),
+        ),
+        backTitle: Text(
+          'MENU',
+          style: TextStyle(
+              color:kCSBWhite,
+              fontWeight: FontWeight.w800
+          ),
+        ),
+        subtitle: _currentCategory.toString().replaceAll('Category.', '').replaceAll('_', ' ').toUpperCase(),
+      ),
+      onGenerateRoute: _getRoute,
+      theme: _kCSBTheme,
+    );
+  }
+
+  /// Function to call when a [Category] is tapped.
+  void _onCategoryTap(Category category) {
+    setState(() {
+      _currentCategory = category;
+    });
+  }
+}
+
+Route<dynamic> _getRoute(RouteSettings settings) {
+//  if (settings.name != '/login') {
+  return null;
+//  }
+
+//  return MaterialPageRoute<void>(
+//    settings: settings,
+//    builder: (BuildContext context) => LoginPage(),
+//    fullscreenDialog: true,
+//  );
+}
+
+final ThemeData _kCSBTheme = _buildCSBTheme();
+
+IconThemeData _customIconTheme(IconThemeData original) {
+  return original.copyWith(color: kCSBWhite);
+}
+
+ThemeData _buildCSBTheme() {
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+    accentColor: kCSBWhite,
+    primaryColor: kCSBDarkBlue,
+    scaffoldBackgroundColor: kCSBBackgroundWhite,
+    cardColor: kCSBBackgroundWhite,
+    textSelectionColor: kCSBDarkBlue,
+    errorColor: kCSBErrorRed,
+    buttonTheme: base.buttonTheme.copyWith(
+      buttonColor: kCSBDarkBlue,
+      textTheme: ButtonTextTheme.normal,
+    ),
+    primaryIconTheme: base.iconTheme.copyWith(color: kCSBGreen),
+    inputDecorationTheme: InputDecorationTheme(
+      border: CutCornersBorder(),
+    ),
+    textTheme: _buildCSBTextTheme(base.textTheme),
+    primaryTextTheme: _buildCSBTextTheme(base.primaryTextTheme),
+    accentTextTheme: _buildCSBTextTheme(base.accentTextTheme),
+    iconTheme: _customIconTheme(base.iconTheme),
+  );
+}
+
+TextTheme _buildCSBTextTheme(TextTheme base) {
+  return base.copyWith(
+    headline: base.headline.copyWith(
+      fontWeight: FontWeight.w500,
+    ),
+    title: base.title.copyWith(
+      fontSize: 18.0,
+    ),
+    caption: base.caption.copyWith(
+      fontWeight: FontWeight.w400,
+      fontSize: 14.0,
+    ),
+    body2: base.body2.copyWith(
+      fontWeight: FontWeight.w500,
+      fontSize: 16.0,
+    ),
+    subtitle: base.subtitle.copyWith(
+      fontWeight: FontWeight.w600,
+      fontSize: 16.0,
+    ),
+  ).apply(
+    fontFamily: 'Montserrat',
+    displayColor: kCSBDarkBlue,
+    bodyColor: kCSBDarkBlue,
+  );
+}
---
+++ lib/model/products_repository.dart
@@ -0,0 +1,99 @@
+// Copyright 2018-present the Flutter authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'product.dart';
+
+class ProductsRepository {
+  static List<Product> loadProducts(Category category) {
+    const allProducts = <Product> [
+      Product(
+        category: Category.home,
+        id: 9,
+        isFeatured: true,
+        name: 'Gilt desk trio',
+        price: 58,
+      ),
+      Product(
+        category: Category.home,
+        id: 10,
+        isFeatured: false,
+        name: 'Copper wire rack',
+        price: 18,
+      ),
+      Product(
+        category: Category.home,
+        id: 11,
+        isFeatured: false,
+        name: 'Soothe ceramic set',
+        price: 28,
+      ),
+      Product(
+        category: Category.home,
+        id: 12,
+        isFeatured: false,
+        name: 'Hurrahs tea set',
+        price: 34,
+      ),
+      Product(
+        category: Category.home,
+        id: 13,
+        isFeatured: true,
+        name: 'Blue stone mug',
+        price: 18,
+      ),
+      Product(
+        category: Category.home,
+        id: 14,
+        isFeatured: true,
+        name: 'Rainwater tray',
+        price: 27,
+      ),
+      Product(
+        category: Category.home,
+        id: 15,
+        isFeatured: true,
+        name: 'Chambray napkins',
+        price: 16,
+      ),
+      Product(
+        category: Category.home,
+        id: 16,
+        isFeatured: true,
+        name: 'Succulent planters',
+        price: 16,
+      ),
+      Product(
+        category: Category.home,
+        id: 17,
+        isFeatured: false,
+        name: 'Quartet table',
+        price: 175,
+      ),
+      Product(
+        category: Category.home,
+        id: 18,
+        isFeatured: true,
+        name: 'Kitchen quattro',
+        price: 129,
+      ),
+    ];
+    if (category == Category.home) {
+      return allProducts;
+    } else {
+      return allProducts.where((Product p) {
+        return p.category == category;
+      }).toList();
+    }
+  }
+}
